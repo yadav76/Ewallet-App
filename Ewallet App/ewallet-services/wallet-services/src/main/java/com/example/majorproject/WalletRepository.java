e093aec83ac8667807d184ad149af5fb9ebb02ef
yadav76 <93885474+yadav76@users.noreply.github.com>
2023-01-25 13:47:00 +0530

    Add files via upload
    First Commit

---
+++ Ewallet App/ewallet-services/wallet-services/src/main/java/com/example/majorproject/WalletRepository.java
@@ -0,0 +1,14 @@
+package com.example.majorproject;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface WalletRepository extends JpaRepository<Wallet,Integer> {
+
+//    @Modifying
+//    @Query(value="UPDATE Wallet w set w.amount = w.amount + :amount where w.userName = :userName",nativeQuery = true)
+//    int updateWallet(String userName,int amount);
+
+    Wallet findByUserName(String userName);
+}
